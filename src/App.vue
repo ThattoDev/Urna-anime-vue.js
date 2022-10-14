d1fef95061814af659881be142f8b45255afc836
ThattoDev <thacito.pfilgueiras@gmail.com>
2022-10-14 11:09:45 -0300

    Componentização do teclado

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    
+    <div class="urna">
+      
+      <Teclado/>
+
+    </div>
   </div>
 </template>
 
@@ -8,9 +12,12 @@
 
 import '@/css/global.css';
 
+import Teclado from '@/components/teclado.vue';
+
 export default {
   name: 'App',
   components: {
+    Teclado
     
   }
 }
@@ -19,5 +26,20 @@ export default {
 <style>
 #app {
   
+  background-color: var(--bg-page);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.urna {
+  width: 70%;
+  height: 70%;
+  background-color: var(--bg-box);
+  padding: 30px;
+  border-radius: 10px;
+  display: flex;
 }
 </style>
---
+++ src/components/teclado.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="urna-teclado">
+
+        <div class="teclado-nemerico">
+            <div class="urna-numero-linha">
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+            </div>
+            <div class="urna-numero-linha">
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+            </div>
+            <div class="urna-numero-linha">
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+            </div>
+            <div class="urna-numero-linha">
+                <button>0</button>
+            </div>
+        </div>
+
+        <div class="teclado-acoes">
+            <button>BRANCO</button>
+            <button>CORRIGE</button>
+            <button>CONFIRMA</button>
+
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+
+import '@/css/global.css';
+
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Teclado'
+}
+
+</script>
+
+<style>
+    .urna-teclado {
+        width: 40%;
+        height: 100%;
+        background-color: var(--box-teclado);
+    }
+
+</style>
